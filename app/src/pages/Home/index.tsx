b445156edcd6f4dee33869846f59048879a1866f
Alexis <dnc.alexis@gmail.com>
2024-05-04 13:44:57 +0200

    Init: add base project (fix lib + tailwind)

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,42 +1,45 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <>
-      <div className="title">
-        <h1>HRnet</h1>
+      <div className="flex items-center justify-center">
+        <h1 className="text-5xl font-bold">HRnet</h1>
       </div>
       <div className="container">
-        <Link to="/employee-list">View Current Employees</Link>
-        <h2>Create Employee</h2>
+        <Link to="/employee-list" className="underline">
+          View Current Employees
+        </Link>
+        <h2 className="text-3xl font-bold">Create Employee</h2>
         <form action="#" id="create-employee">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <Input type="text" id="first-name" />
 
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <Input type="text" id="last-name" />
 
           <label htmlFor="date-of-birth">Date of Birth</label>
-          <input id="date-of-birth" type="text" />
+          <Input id="date-of-birth" type="text" />
 
           <label htmlFor="start-date">Start Date</label>
-          <input id="start-date" type="text" />
+          <Input id="start-date" type="text" />
 
           <fieldset className="address">
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <Input id="street" type="text" />
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <Input id="city" type="text" />
 
             <label htmlFor="state">State</label>
             <select name="state" id="state"></select>
 
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <Input id="zip-code" type="number" />
 
             <label htmlFor="department">Department</label>
             <select name="department" id="department">
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,9 +2,9 @@
 export default {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
@@ -51,6 +51,18 @@ export default {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
       },
       borderRadius: {
         lg: "var(--radius)",
